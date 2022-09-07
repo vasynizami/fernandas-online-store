22554faf2321e4313ce6f3e86606bedb7b7f2c23
Gabrielle <eastongabrielle@gmail.com>
2022-09-07 08:33:22 -0400

    generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/shop', to: 'products#index'
+  get '/shop/:id', to: 'products#show'
+  post '/orders/submit', to: 'orders#submit'
+
 
   # Defines the root path route ("/")
   # root "articles#index"
