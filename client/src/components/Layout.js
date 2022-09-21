845d6a2b804ef5bda3905bda7afd0c6bf88f2194
Gabrielle <eastongabrielle@gmail.com>
2022-09-20 20:51:01 -0400

    basic structure created for front end

---
+++ client/src/components/Layout.js
@@ -0,0 +1,46 @@
+// import {
+//   AppBar,
+//   Badge,
+//   Box,
+//   Button,
+//   Container,
+//   CssBaseline,
+//   Divider,
+//   Drawer,
+//   IconButton,
+//   InputBase,
+//   Link,
+//   List,
+//   ListItem,
+//   ListItemText,
+//   Menu,
+//   MenuItem,
+//   Switch,
+//   Toolbar,
+//   Typography,
+//   useMediaQuery,
+// } from '@mui/material';
+// import MenuIcon from '@mui/icons-material/Menu';
+// import SearchIcon from '@mui/icons-material/Search';
+// import CancelIcon from '@mui/icons-material/Cancel';
+// import Head from 'next/head';
+// import NextLink from 'next/link';
+// import classes from '../utils/classes';
+// import { useContext, useEffect, useState } from 'react';
+// import { Store } from '../utils/Store';
+// import jsCookie from 'js-cookie';
+// import { useRouter } from 'next/router';
+// import axios from 'axios';
+// import { useSnackbar } from 'notistack';
+// import { getError } from '../utils/error';
+
+//functional component accepts three parameters 
+
+export default function Layout({ title, description, children }) {
+
+  return (
+    <>
+     <div>Layout</div>
+    </>
+  );
+}
