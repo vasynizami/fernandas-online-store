9e757aa6af4f7a9d37874485d58378b50640957c
Gabrielle <eastongabrielle@gmail.com>
2022-10-22 12:10:15 -0400

    imported api call function

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import axios from "axios";
+import Shop from './components/Shop';
+import {useEffect, useState} from "react";
+import { getAllProducts } from './services/api-helper';
+
+
+// const API_URL = "";
+// function getAPIData(){
+//   return axios.get(API_URL).then((response) => response.data)
+// }
+
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    let mounted = true;
+    getAllProducts().then((items) => {
+if(mounted) {
+  setProducts(items);
+}
+    });
+    return () => (mounted = false);
+  }, []);
+
   return (
     <div className="App">
-      <Title />
-      <Shop />
+      {/* <Title /> */}
+      <Shop products = {products}/>
     </div>
   );
 }
